4e17e81595aa17eec0061f28f01c8c37d58d5dc1
VianneyR4 <vianneyrwicha2017@gmail.com>
2021-09-25 22:46:34 +0200

    Fix:: Upload file issue

--- src/middlewares/uploadFile.js
+++ src/middlewares/uploadFile.js
@@ -1,23 +1,68 @@
 import multer from 'multer';
-const maxSize = 2 * 1024 * 1024;
+import fs from 'fs';
+import randomstring from 'randomstring';
+import path from 'path';
 
+
+// function destination(file) {
+//     let mime = file.mimetype;
+//     let arr = mime.split('/');
+//     let type = arr[0];
+//     let start = `/static/uploads/${type}/`;
+//     let midlle = start + (new Date().getFullYear()) + "/";
+//     let final = midlle + (new Date().getMonth() + 1);
+//     let dest = [start, midlle, final];
+//     for (var i = 0; i < dest.length; i++) {
+//         if (!fs.existsSync(dest[i])) {
+//             fs.mkdir(dest[i], e => {
+//                 if (e) throw e;
+//             });
+//         }
+//     }
+
+//     return final;
+// }
+
+function getType(src) {
+    let mp3 = ["mpeg"];
+    if (mp3.indexOf(src) > -1) {
+        return "mp3";
+    }
+    return src;
+}
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, '/static/uploads');
+        // send file path ...
+        req.newFilePath= path.join(__dirname, '../../static/uploads');
+        cb(null, req.newFilePath);
     },
     filename: function (req, file, cb) {
-        cb(null, Date.now() + "--UJATCARE--" + file.originalname);
+        let mime = file.originalname;
+        let getTypeExtension = mime.split(".");
+        let CountPoint = getTypeExtension.length;
+        let newFileName = randomstring.generate() + `.${getTypeExtension[CountPoint-1]}`;
+        // send file name ...
+        req.newFileName = newFileName;
+        cb(null, newFileName);
     }
-});  
+});
 
 const fileFilter = (req, file, cb) => {
-    if((file.mimetype).includes('jpeg') || (file.mimetype).includes('png') || (file.mimetype).includes('jpg')){
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/JPEG' || file.mimetype === 'image/png' || file.mimetype === 'image/PNG' ) {
         cb(null, true);
-    } else{
-        cb(null, false);
+    } else if (file.mimetype === 'audio/mp3' || file.mimetype === 'audio/wav' || file.mimetype === 'audio/mpeg') {
+        cb(null, true);
+    } else {
+        cb(null, true);
     }
+
+    console.log("__Uploaded_file___=>", file);
 };
 
-let upload = multer({ storage: storage, limits: { fileSize: maxSize }, fileFilter: fileFilter });
-
-export default upload;
+export default multer({
+    "storage": storage,
+    "limits": {
+        fileSize: 1024 * 1024 * 20
+    },
+    "fileFilter": fileFilter
+});
